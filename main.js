cbf8da234c6c17ef28327a4c966c01f484e0d970
zhouanbo <zhouanbo@gmail.com>
2016-03-24 13:09:16 -0400

    rebranding

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const globalShortcut = electron.globalShortcut;
+const Menu = electron.Menu;
+const shell = electron.shell;
 
+let menu;
+let template;
 let mainWindow;
 
 function createWindow () {
@@ -21,6 +25,117 @@ function createWindow () {
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+  
+  //App menu  
+  if (process.platform === 'darwin') {
+    template = [{
+      label: 'PipelineDog',
+      submenu: [{
+        label: 'About PipelineDog',
+        selector: 'orderFrontStandardAboutPanel:',
+        click() {
+          shell.openExternal('http://pipeline.dog');
+        }
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Hide PipelineDog',
+        accelerator: 'Command+H',
+        selector: 'hide:'
+      }, {
+        label: 'Hide Others',
+        accelerator: 'Command+Shift+H',
+        selector: 'hideOtherApplications:'
+      }, {
+        label: 'Show All',
+        selector: 'unhideAllApplications:'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() {
+          app.quit();
+        }
+      }]
+    }, {
+      label: 'View',
+      submenu: [{
+        label: 'Toggle Full Screen',
+        accelerator: 'Ctrl+Command+F',
+        click() {
+          mainWindow.setFullScreen(!mainWindow.isFullScreen());
+        }
+      }]
+    }, {
+      label: 'Window',
+      submenu: [{
+        label: 'Minimize',
+        accelerator: 'Command+M',
+        selector: 'performMiniaturize:'
+      }, {
+        label: 'Close',
+        accelerator: 'Command+W',
+        selector: 'performClose:'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Bring All to Front',
+        selector: 'arrangeInFront:'
+      }]
+    }, {
+      label: 'Help',
+      submenu: [{
+        label: 'Learn More',
+        click() {
+          shell.openExternal('http://pipeline.dog');
+        }
+      }, {
+        label: 'Documentation',
+        click() {
+          shell.openExternal('http://pipeline.dog/documentation');
+        }
+      }]
+    }];
+
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+  } else {
+    template = [{
+      label: '&File',
+      submenu: [{
+        label: '&Close',
+        accelerator: 'Ctrl+W',
+        click() {
+          mainWindow.close();
+        }
+      }]
+    }, {
+      label: '&View',
+      submenu: [{
+        label: 'Toggle &Full Screen',
+        accelerator: 'F11',
+        click() {
+          mainWindow.setFullScreen(!mainWindow.isFullScreen());
+        }
+      }]
+    }, {
+      label: 'Help',
+      submenu: [{
+        label: 'Learn More',
+        click() {
+          shell.openExternal('http://pipeline.dog');
+        }
+      }, {
+        label: 'Documentation',
+        click() {
+          shell.openExternal('http://pipeline.dog/documentation');
+        }
+      }]
+    }];
+    menu = Menu.buildFromTemplate(template);
+    mainWindow.setMenu(menu);
+  }
 }
 
 app.on('ready', createWindow);
@@ -36,4 +151,7 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
   }
+  
+  
+   
 });
