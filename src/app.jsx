024009d78678f444515e7caf2268e2590f911e57
zhouanbo <zhouanbo@gmail.com>
2016-04-12 10:04:38 -0400

    app menu separation

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var remote = require('remote');
-var Menu = remote.Menu;
-var shell = remote.shell;
-var application = remote.app;
 var dialog = remote.require('dialog');
-var spawn = require('child_process').spawn;
+var Menu = remote.Menu;
 var path = require('path');
 
 var Util = require('./lib/util');
+var Menus = require('./lib/menus')
 var CodeParse = require('./lib/codeParse');
 var FileOperation = require('./lib/fileOperation');
 var HeaderPanel = require('./lib/headerPanel');
@@ -52,209 +50,13 @@ var MainComponent = React.createClass({
 
   componentDidMount: function() {
     var app = this;
+    var template = {};
     //App menu  
-    var template = [
-      {
-        label: 'File',
-        submenu: [
-          {
-            label: 'New Project',
-            accelerator: 'CmdOrCtrl+N',
-            click: function(item, focusedWindow) {
-              FileOperation.newProject(app);
-            }
-          },
-          {
-            label: 'Open Project',
-            accelerator: 'CmdOrCtrl+O',
-            click: function(item, focusedWindow) {
-              FileOperation.openProject(app);
-            }
-          },
-          {
-            label: 'Save Project',
-            accelerator: 'CmdOrCtrl+S',
-            click: function(item, focusedWindow) {
-              FileOperation.saveProject(app);
-            }
-          },
-          {
-            label: 'Run Command',
-            accelerator: 'CmdOrCtrl+Alt+R',
-            click: function(item, focusedWindow) {
-              app.runCode();
-            }
-          },
-          {
-            label: 'Export Command',
-            accelerator: 'CmdOrCtrl+E',
-            click: function(item, focusedWindow) {
-              app.exportCode();
-            }
-          },
-          
-        ]
-      },
-      {
-        label: 'Edit',
-        submenu: [
-          {
-            label: 'Undo',
-            accelerator: 'CmdOrCtrl+Z',
-            role: 'undo'
-          },
-          {
-            label: 'Redo',
-            accelerator: 'Shift+CmdOrCtrl+Z',
-            role: 'redo'
-          },
-          {
-            type: 'separator'
-          },
-          {
-            label: 'Cut',
-            accelerator: 'CmdOrCtrl+X',
-            role: 'cut'
-          },
-          {
-            label: 'Copy',
-            accelerator: 'CmdOrCtrl+C',
-            role: 'copy'
-          },
-          {
-            label: 'Paste',
-            accelerator: 'CmdOrCtrl+V',
-            role: 'paste'
-          },
-          {
-            label: 'Select All',
-            accelerator: 'CmdOrCtrl+A',
-            role: 'selectall'
-          },
-        ]
-      },
-      {
-        label: 'View',
-        submenu: [
-          {
-            label: 'Reload',
-            accelerator: 'CmdOrCtrl+R',
-            click: function(item, focusedWindow) {
-              if (focusedWindow)
-                focusedWindow.reload();
-            }
-          },
-          {
-            label: 'Toggle Full Screen',
-            accelerator: (function() {
-              if (process.platform == 'darwin')
-                return 'Ctrl+Command+F';
-              else
-                return 'F11';
-            })(),
-            click: function(item, focusedWindow) {
-              if (focusedWindow)
-                focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-            }
-          },
-          {
-            label: 'Toggle Developer Tools',
-            accelerator: (function() {
-              if (process.platform == 'darwin')
-                return 'Alt+Command+I';
-              else
-                return 'Ctrl+Shift+I';
-            })(),
-            click: function(item, focusedWindow) {
-              if (focusedWindow)
-                focusedWindow.toggleDevTools();
-            }
-          },
-        ]
-      },
-      {
-        label: 'Window',
-        role: 'window',
-        submenu: [
-          {
-            label: 'Minimize',
-            accelerator: 'CmdOrCtrl+M',
-            role: 'minimize'
-          },
-          {
-            label: 'Close',
-            accelerator: 'CmdOrCtrl+W',
-            role: 'close'
-          },
-        ]
-      },
-      {
-        label: 'Help',
-        role: 'help',
-        submenu: [
-          {
-            label: 'Learn More',
-            click: function() { shell.openExternal('http://pipeline.dog') }
-          },
-        ]
-      },
-    ];
-
     if (process.platform == 'darwin') {
-      template.unshift({
-        label: "PipelineDog",
-        submenu: [
-          {
-            label: 'About PipelineDog',
-            role: 'about'
-          },
-          {
-            type: 'separator'
-          },
-          {
-            label: 'Services',
-            role: 'services',
-            submenu: []
-          },
-          {
-            type: 'separator'
-          },
-          {
-            label: 'Hide PipelineDog',
-            accelerator: 'Command+H',
-            role: 'hide'
-          },
-          {
-            label: 'Hide Others',
-            accelerator: 'Command+Alt+H',
-            role: 'hideothers'
-          },
-          {
-            label: 'Show All',
-            role: 'unhide'
-          },
-          {
-            type: 'separator'
-          },
-          {
-            label: 'Quit',
-            accelerator: 'Command+Q',
-            click: function() { application.quit(); }
-          },
-        ]
-      });
-      // Window menu.
-      template[3].submenu.push(
-        {
-          type: 'separator'
-        },
-        {
-          label: 'Bring All to Front',
-          role: 'front'
-        }
-      );
+      template = Menus.macMenu;
+    } else {
+      template = Menus.winMenu;
     }
-
     var menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
   },
---
+++ src/menus.js
@@ -0,0 +1,351 @@
+var remote = require('remote');
+var application = remote.app;
+var shell = remote.shell;
+ 
+var Menus = {
+  macMenu: [
+    {
+      label: "PipelineDog",
+      submenu: [
+        {
+          label: 'About PipelineDog',
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Services',
+          role: 'services',
+          submenu: []
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Hide PipelineDog',
+          accelerator: 'Command+H',
+          role: 'hide'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Alt+H',
+          role: 'hideothers'
+        },
+        {
+          label: 'Show All',
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function() { application.quit(); }
+        },
+      ]
+    },
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'New Project',
+          accelerator: 'CmdOrCtrl+N',
+          click: function(item, focusedWindow) {
+            FileOperation.newProject(app);
+          }
+        },
+        {
+          label: 'Open Project',
+          accelerator: 'CmdOrCtrl+O',
+          click: function(item, focusedWindow) {
+            FileOperation.openProject(app);
+          }
+        },
+        {
+          label: 'Save Project',
+          accelerator: 'CmdOrCtrl+S',
+          click: function(item, focusedWindow) {
+            FileOperation.saveProject(app);
+          }
+        },
+        {
+          label: 'Run Command',
+          accelerator: 'CmdOrCtrl+Alt+R',
+          click: function(item, focusedWindow) {
+            app.runCode();
+          }
+        },
+        {
+          label: 'Export Command',
+          accelerator: 'CmdOrCtrl+E',
+          click: function(item, focusedWindow) {
+            app.exportCode();
+          }
+        },
+        
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
+        },
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Ctrl+Command+F';
+            else
+              return 'F11';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Alt+Command+I';
+            else
+              return 'Ctrl+Shift+I';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.toggleDevTools();
+          }
+        },
+      ]
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        },
+      ]
+    },
+    {
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: function() { shell.openExternal('http://pipeline.dog') }
+        },
+      ]
+    },
+  ],
+
+  winMenu: [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'New Project',
+          accelerator: 'CmdOrCtrl+N',
+          click: function(item, focusedWindow) {
+            FileOperation.newProject(app);
+          }
+        },
+        {
+          label: 'Open Project',
+          accelerator: 'CmdOrCtrl+O',
+          click: function(item, focusedWindow) {
+            FileOperation.openProject(app);
+          }
+        },
+        {
+          label: 'Save Project',
+          accelerator: 'CmdOrCtrl+S',
+          click: function(item, focusedWindow) {
+            FileOperation.saveProject(app);
+          }
+        },
+        {
+          label: 'Run Command',
+          accelerator: 'CmdOrCtrl+Alt+R',
+          click: function(item, focusedWindow) {
+            app.runCode();
+          }
+        },
+        {
+          label: 'Export Command',
+          accelerator: 'CmdOrCtrl+E',
+          click: function(item, focusedWindow) {
+            app.exportCode();
+          }
+        },
+        
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
+        },
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Ctrl+Command+F';
+            else
+              return 'F11';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: (function() {
+            if (process.platform == 'darwin')
+              return 'Alt+Command+I';
+            else
+              return 'Ctrl+Shift+I';
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow)
+              focusedWindow.toggleDevTools();
+          }
+        },
+      ]
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Bring All to Front',
+          role: 'front'
+        }
+      ]
+    },
+    {
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: function() { shell.openExternal('http://pipeline.dog') }
+        },
+      ]
+    },
+  ],
+  
+}
+
+module.exports = Menus;
